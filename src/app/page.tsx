4ba78aaa16ee69d4f5add8eed0fc1c59aee18890
AliReza99 <alirezawbhr@gmail.com>
2024-11-22 12:04:47 +0330

    feat: connect to mongodb atlas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-// import { db } from "@/lib/mongodb";
+import { db } from "@/lib/mongodb";
 
 export default async function Home() {
-  // const logs = await db.collection("logs").find({}).toArray();
+  const logs = await db.collection("logs").find({}, { limit: 10 }).toArray();
 
-  // console.log(`[logs] `, logs);
+  console.log(`[logs] `, logs);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <span>logs:</span>
-      {/* <pre>{JSON.stringify(logs, null, 2)}</pre> */}
+      <pre>{JSON.stringify(logs, null, 2)}</pre>
     </div>
   );
 }
