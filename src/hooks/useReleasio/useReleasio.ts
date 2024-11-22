8c64c5ffb53820953e6f927252dfd54b5a6eae42
AliReza99 <alirezawbhr@gmail.com>
2024-11-22 16:26:21 +0330

    feat: update useReleasio

--- src/hooks/useReleasio/useReleasio.ts
+++ src/hooks/useReleasio/useReleasio.ts
@@ -6,13 +6,45 @@ export type UseReleasioParams = {
 };
 
 export function useReleasio({ token, commit }: UseReleasioParams) {
-  const { data, error, isLoading } = useSWR(`${token}:${commit}`, async (v) => {
-    // console.log(`[v] `, v);
+  const { data, error } = useSWR(
+    `${token}:${commit}`,
+    async (v) => {
+      const res = await fetch(
+        `https://releasio.vercel.app/api/tokens/${token}/logs`
+      );
+      const data = await res.json();
+      return data.data as {
+        changes: string[];
+        commit: string;
+        date: string;
+        token: string;
+        version: string;
+      }[];
+    },
+    {
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+      errorRetryCount: 5,
+      errorRetryInterval: 10000,
+      refreshInterval: 20000,
+      // isPaused: () => alreadyWarned,
+      refreshWhenHidden: false,
+      refreshWhenOffline: false,
+      // onSuccess: (data) => {
+      //   const { version } = data;
+      //   if (!version || version === buildVersion) return;
+      //   alreadyWarned = true;
+      //   showReloadNotification(
+      //     t("Update available. Reload to apply new changes.")
+      //   );
+      // },
+    }
+  );
 
-    const res = await fetch(
-      `https://releasio.vercel.app/api/token/${token}/logs`
-    );
-    const data = await res.json();
-    return data;
-  });
+  return {
+    updateAvailable: false,
+    version: data?.[0]?.version,
+    changelogs: data,
+    error: error,
+  };
 }
