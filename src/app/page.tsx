a6ede61a4d0c9f69edd5f9ff86729c990660e8d3
AliReza99 <alirezawbhr@gmail.com>
2024-11-22 13:24:40 +0330

    feat: remove logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import { db } from "@/lib/mongodb";
 export default async function Home() {
   const logs = await db.collection("logs").find({}, { limit: 10 }).toArray();
 
-  console.log(`[logs] `, logs);
-
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 gap-4">
       <span>logs:</span>
       <pre>{JSON.stringify(logs, null, 2)}</pre>
     </div>
