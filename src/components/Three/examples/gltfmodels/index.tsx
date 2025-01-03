2f686658561845aab0d1bfcf16aecd7616cc77a8
AliReza99 <alirezawbhr@gmail.com>
2025-01-03 16:34:03 +0330

    feat: add example of EventEmitter

--- src/components/Three/examples/gltfmodels/index.tsx
+++ src/components/Three/examples/gltfmodels/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import { GLTFLoader, DRACOLoader } from "three/examples/jsm/Addons.js";
+import "@/utils/EventEmitter";
 
 // @refresh reset
 
