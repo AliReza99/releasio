e144eb82a8613eb1212c5f06e3858f75597b9807
AliReza99 <alirezawbhr@gmail.com>
2025-01-09 21:56:31 +0330

    feat: enable glsl import

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.glsl$|\.frag$|\.vert$/i,
+      use: ["raw-loader"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
