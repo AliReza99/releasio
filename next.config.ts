f7f059890d1395f7dd24268afc2b50db21850cb0
AliReza99 <alirezawbhr@gmail.com>
2025-01-10 22:51:41 +0330

    feat: disable react strict

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.glsl$|\.frag$|\.vert$/i,
