ba588eeb155d35bcb35a7f1dd8616db61bcbc5e8
AliReza99 <alirezawbhr@gmail.com>
2025-02-15 16:33:43 +0330

    feat: update to log errors

--- server/middlewares/loggerMiddleware/index.ts
+++ server/middlewares/loggerMiddleware/index.ts
@@ -4,7 +4,21 @@ export async function onRequestLogger(request: FastifyRequest) {
   request.log.info(`[req] ${request.method}: ${request.url}`);
 }
 
-export async function onResponseLogger(request: FastifyRequest) {
+export async function onResponseLogger(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  if (reply.statusCode >= 500) {
+    request.log.error(
+      {
+        request: `${request.method}: ${request.url}`,
+        parameters: request.params,
+      },
+      `[res] ${request.method}: ${request.url}`
+    );
+    return;
+  }
+
   request.log.info(
     {
       request: `${request.method}: ${request.url}`,
