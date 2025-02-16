c7cfe7baaea9e2f3ddcfceca944635605380005f
AliReza99 <alirezawbhr@gmail.com>
2025-02-16 09:35:38 +0330

    feat: add type helpers

---
+++ array-includes.d.ts
@@ -0,0 +1,31 @@
+declare namespace TSReset {
+  type NonFalsy<T> = T extends false | 0 | "" | null | undefined | 0n
+    ? never
+    : T;
+
+  type WidenLiteral<T> = T extends string
+    ? string
+    : T extends number
+    ? number
+    : T extends boolean
+    ? boolean
+    : T extends bigint
+    ? bigint
+    : T extends symbol
+    ? symbol
+    : T;
+}
+
+interface ReadonlyArray<T> {
+  includes(
+    searchElement: T | (TSReset.WidenLiteral<T> & {}),
+    fromIndex?: number
+  ): boolean;
+}
+
+interface Array<T> {
+  includes(
+    searchElement: T | (TSReset.WidenLiteral<T> & {}),
+    fromIndex?: number
+  ): boolean;
+}
